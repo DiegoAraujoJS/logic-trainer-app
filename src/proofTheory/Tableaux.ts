ffc7fe651d0a832e03df302fd1f03cbaf2d7fe27
DiegoAraujoJS <diegolaraujo96@gmail.com>
2021-08-28 02:02:50 -0300

    buggy isClosed Tableaux

--- src/proofTheory/Tableaux.ts
+++ src/proofTheory/Tableaux.ts
@@ -25,7 +25,7 @@ export default class Tableaux<T extends FTypes> implements ITableaux<T>{
                 return [new Tableaux(this.formula.formula[0]), new Tableaux(this.formula.formula[2])]
             }
         } else if (this.formula.is_negation(this.formula.formula)){
-            return [new Tableaux(this.formula.formula[1]), null]
+            return [null, null]
         } else {
             return [null, null]
         }
@@ -37,6 +37,28 @@ export default class Tableaux<T extends FTypes> implements ITableaux<T>{
         if (this.right) this.right.log()
     }
 
+    get isClosed(): boolean {
+        let check = false
+        let branches = this.get_branches
+        const negation_branch = branches.find(branch => branch.some(formula => formula.is_negation(formula.formula)))
+        while (negation_branch){
+            
+            const negation = negation_branch.find(formula => formula.is_negation(formula.formula))
+            console.log(negation)
+            const contradiction = negation_branch.find(formula => formula == negation?.left)
+            if (contradiction){
+                check = true
+                break
+            } else {
+                branches = branches.filter(branch => branch != negation_branch)
+                const filtered_negation_branch = negation_branch.filter(formula => formula != negation)
+                branches = [...branches, filtered_negation_branch]
+            }
+        
+        }
+        return check
+    }
+
     private get_branches_execution(tree: Tableaux<FTypes>, branches: any[], trace: null | Formula<FTypes>[] = null): void {
         trace = Array.isArray(trace) ? [...trace, tree.formula] : [tree.formula]
         const this_branches_l = tree.left ? this.get_branches_execution(tree.left, branches, trace) : []
@@ -45,8 +67,8 @@ export default class Tableaux<T extends FTypes> implements ITableaux<T>{
             branches.push(trace)
         }
     }
-    private get get_branches(): Formula<FTypes>[] {
-        let branches: Formula<FTypes>[] = []
+    get get_branches(): Formula<FTypes>[][] {
+        let branches: Formula<FTypes>[][] = []
         this.get_branches_execution(this, branches)
         return branches
     }
@@ -63,12 +85,23 @@ export default class Tableaux<T extends FTypes> implements ITableaux<T>{
 // T.left.right =  new Tableaux(new Formula<Basic>('S'))
 
 const P = new Formula<Basic>('P', "Atomic")
+const not_P = P.negation()
 const Q = new Formula<Basic>('Q', "Atomic")
+const R = new Formula<Basic> ('R', "Atomic")
+const not_Q = Q.negation()
 const P_or_Q = P.disyunction(Q)
-const T = new Tableaux(P_or_Q.conjunction(P))
+const T = new Tableaux(P.conjunction(R).conjunction(Q).conjunction(not_Q))
+console.log(T.get_branches)
+console.log(T.isClosed)
 
-T.log()
 /// [ [], 1] --> 2
+
+//      P & (P v Q)
+//      P
+//      (P v Q)
+//          /\
+//         /  \
+//        P    Q
 
 let array = [1, [[2]], [1]]
 
